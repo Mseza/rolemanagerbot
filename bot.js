d251eb0b1ba32919566a2ae13b10a050769cb9e4
Mseza <l_gottar@orange.fr>
2017-11-02 14:19:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
   let guild = message.member.guild;
-  let programmeur = guild.roles.find("name", 'Programmeur');
-  let graphiste = guild.roles.find("name", 'Graphiste');
-  let modeliseur = guild.roles.find("name", 'Modeleur');
-  let soundDesigner = guild.roles.find("name", 'SoundDesigner');
+  let programmeur = guild.roles.find("name", 'Programmeurs');
+  let graphiste = guild.roles.find("name", 'Graphistes');
+  let modeliseur = guild.roles.find("name", 'Modeleurs');
+  let soundDesigner = guild.roles.find("name", 'Sound Designers');
+  let levelDesigner = guild.roles.find("name", 'Level Designers');
 
   let prefixe = message.content.split(" ")[0];
 
@@ -24,6 +25,7 @@ client.on('message', message => {
       message.member.removeRole(graphiste);
       message.member.removeRole(modeliseur);
       message.member.removeRole(soundDesigner);
+      message.member.removeRole(levelDesigner);
 
       if (args === 'programmeur') {
         message.member.addRole(programmeur);
@@ -41,7 +43,10 @@ client.on('message', message => {
         message.member.addRole(soundDesigner);
         message.reply("vous êtes désormais un sound designer");
       }
-
+      else if (args === 'level designer') {
+        message.member.addRole(levelDesigner);
+        message.reply("vous êtes désormais un level designer");
+      }
       else {
         message.reply("vous avez mentionné un rôle non valide")
       }
